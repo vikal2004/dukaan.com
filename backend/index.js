d366a24ced5615e7bcfd4cca5b05966e5d3cc5b9
vikal singh <114324236+vikal2004@users.noreply.github.com>
2025-05-02 22:02:38 +0530

    added-index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
-console.log("hello guys")
+import express from "express"
+import dotenv from "dotenv"
+const app=express();
+import bodyParser from "body-parser"
+import cors from "cors"
+import { connectdb } from "./db/index.js";
+
+dotenv.config();
+const PORT=process.env.PORT;
+
+app.use(cors({
+    origin:"http://localhost:5147", 
+    credentials:true
+}))
+app.use(express.json());
+app.use(bodyParser.json());
+
+connectdb();
+
+app.get("/", (req, res)=>{
+    res.send("hi guys kaise ho")
+})
+
+app.listen(PORT, ()=>{
+    console.log(`your app is listening to PORT no ${PORT}`)
+})
